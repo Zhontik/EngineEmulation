ae89ded9d5e347ac551912fd1c3cec004c1c6d2b
Alex <pichuev.alex@yandex.ru>
2021-02-24 16:28:56 +0700

    Реализован интерфейс для стендов

---
+++ EngineEmulate/ITestStand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineEmulate
+{
+    interface ITestStand
+    {
+        /// <summary>
+        /// Test target engine using internal test;
+        /// </summary>
+        /// <param name="outTemperature"> Temperature outside. </param>
+        /// <param name="engine"> Engine to test. </param>
+        void StartEngineTesting(int outTemperature, IEngine engine);
+
+    }
+}
