e8a6938f6d030a893a1e26fdbbdd12d0d792f607
Alex <pichuev.alex@yandex.ru>
2021-02-24 16:30:17 +0700

    Внесены правки из-за проблем с совместимостью с интерфейсом тестового стенда

--- EngineEmulate/IEngine.cs
+++ EngineEmulate/IEngine.cs
@@ -6,8 +6,20 @@ namespace EngineEmulate
 {
     interface IEngine
     {
-        void Start(int outTemperature);
 
+        int temperatureOverheat { get; set; }
+        double tnow { get; set; }
+
+        /// <summary>
+        /// Takes a time step of 1 second;
+        /// Change speed and temerature using internal rules;
+        /// </summary>
+        /// <param name="outTemperature"> Temperature outside. </param>
+        void TimeStep(int outTemperature);
+        
+        /// <summary>
+        /// Print all the parametres about the engine.
+        /// </summary>
         void Info();
     }
 }
